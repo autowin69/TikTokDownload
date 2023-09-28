43307e3ce14105b49fb15e79dea1f255fa7b67f3
Hoa <getmoneykhmt3@gmail.com>
2023-09-28 14:48:20 +0700

    reboot

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import Util
 import time, requests
 if __name__ == '__main__':
@@ -5,6 +7,7 @@ if __name__ == '__main__':
     cmd = Util.Command()
     config = cmd.config_dict
     dyheaders = cmd.dyheaders
+    cnt=0
     while 1:
         try:
             rs = requests.get("https://mazon.click/api/douyin/user/crawl/get").json()
@@ -12,4 +15,8 @@ if __name__ == '__main__':
                 Util.asyncio.run(Util.Profile(config, dyheaders).get_Profile(rs['url']))
         except:
             pass
-        time.sleep(3)
+        time.sleep(5)
+        cnt+=1
+        if(cnt>86400/5):
+            os.system("reboot")
+            break
